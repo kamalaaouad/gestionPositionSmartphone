e6f41ce0cf2f33c4d4c728f656f8b22f9f1450b8
Utilisateur <kammalaaouad1999@gmail.com>
2022-01-17 08:50:12 +0100

    application gestion smartphone and position server Ejb and app web

---
+++ GestionPositions/ejbModule/ma/entities/ChartDto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ma.entities;
+
+/**
+ *
+ * @author Toufiq
+ */
+public class ChartDto {
+    
+	 private String key;
+	    private Long value;
+
+	    public ChartDto(String key, Long long1) {
+	        this.key = key;
+	        this.value = long1;
+	    }
+
+		@Override
+		public String toString() {
+			return "ChartDto [key=" + key + ", value=" + value + "]";
+		}
+	    
+    
+}
